bc2bc844b7192c9d43e15839386e220eab70792e
Dat0309 <anhtheboy011@gmail.com>
2022-05-17 17:08:31 +0700

    Update

--- BT7_NHOM8/test.py
+++ BT7_NHOM8/test.py
@@ -11,48 +11,28 @@ data_frame = pd.read_csv(
 data_features = data_frame.values[:,1:-1]
 data_labels = data_frame.values[:,-1]
 
-filename = 'RFR_model.sav'
+filename = 'rbf_model.sav'
 
 loaded_model = pickle.load(open(filename, 'rb'))
-result1 = loaded_model.score(data_features, data_labels)
-print("Test File Test1_MeanData_ML with Random Forest model: %.2f%% " % ((result1)*100))
+result1 = loaded_model.predict(data_features)
+
+count = 0 
+for i in range(len(result1)):
+    if int(result1[i]) == int(data_labels[i]):
+        count+=1
+
+print("Test File Test1_MeanData_ML with Random Forest model: %.2f%% " % ((count/len(data_labels))*100))
 
 data_frame = pd.read_csv(
     "TestData//Test2_MeanData_ML.csv")
 
 data_features = data_frame.values[:,1:-1]
 data_labels = data_frame.values[:,-1]
+result2 = loaded_model.predict(data_features)
 
-filename = 'RFR_model.sav'
+count = 0 
+for i in range(len(result1)):
+    if int(result2[i]) == int(data_labels[i]):
+        count+=1
 
-loaded_model = pickle.load(open(filename, 'rb'))
-result2 = loaded_model.score(data_features, data_labels)
-print("Test File Test2_MeanData_ML with Random Forest model: %.2f%% " % ((result2)*100))
-
-print("Test ratio model with random forest: %.2f%% " % (((result1+result2)/2)*100))
-
-data_frame = pd.read_csv(
-    "TestData//Test1_MeanData_ML.csv")
-
-data_features = data_frame.values[:,1:-1]
-data_labels = data_frame.values[:,-1]
-
-filename = 'rbf_model.sav'
-
-loaded_model = pickle.load(open(filename, 'rb'))
-result3 = loaded_model.score(data_features, data_labels)
-print("Test File Test2_MeanData_ML with RBF model: %.2f%% " % ((result3)*100))
-
-data_frame = pd.read_csv(
-    "TestData//Test2_MeanData_ML.csv")
-
-data_features = data_frame.values[:,1:-1]
-data_labels = data_frame.values[:,-1]
-
-filename = 'rbf_model.sav'
-
-loaded_model = pickle.load(open(filename, 'rb'))
-result4 = loaded_model.score(data_features, data_labels)
-print("Test File Test2_MeanData_ML with RBF model: %.2f%% " % ((result4)*100))
-
-print("Test ratio model with RBF: %.2f%% " % (((result3+result4)/2)*100))
+print("Test File Test2_MeanData_ML with Random Forest model: %.2f%% " % ((count/len(data_labels))*100))
